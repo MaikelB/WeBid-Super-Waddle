30636ff194aceced54360619002139c6371ea1ae
MaikelB <maikel.bolderdijk@student.hu.nl>
2016-01-26 12:08:03 +0100

    Initial setup

---
+++ includes/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+$DbHost	 = "localhost";
+$DbDatabase = "webid";
+$DbUser	 = "root";
+$DbPassword = "";
+$DBPrefix	= "webid_";
+$main_path	= "C:\\Users\\MaikelB\\Source\\Repos\\WeBid-Super-Waddle\\";
+$MD5_PREFIX = "96ea194f4abfec0b3c4dbb2ed964c976";
+?>
